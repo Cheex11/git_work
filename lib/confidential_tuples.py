9785f2239aa57fcb166cd899e0bddb9ff80f4ac4
Cody Wilson <Cheex11@gmail.com>
2016-05-02 18:03:52 -0500

    tet
    again

---
+++ lib/confidential_tuples.py
@@ -0,0 +1,22 @@
+tups = ( 	("""gets_replaces""","""replacement"""),
+			("""datasource="#application.ds#""","""datasource=#GIT_protected_system_variable#"""),
+
+
+
+
+			("""nobot""","""GIT_protected_password_to_send_file"""),
+
+
+#DB Tabless
+			("""FROM alarm""","""FROM GIT_tables_that_holds_alerts"""),
+			("""LEFT JOIN alarm""","""FROM GIT_tables_that_holds_alerts"""),
+			("""INNER JOIN alarm""","""FROM GIT_tables_that_holds_alerts"""),
+
+
+#Various Variables
+			("""start_date""","""GIT_begin_date"""),
+			("""end_date""","""GIT_finish_date"""),
+
+
+
+		)
